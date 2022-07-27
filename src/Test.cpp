677894dd2504177e93734279bf00afb5aeee8013
LRLVEC <mjdyx@outlook.com>
2022-07-27 11:25:14 +0800

    add submodule tiny-stl

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,4 +4,15 @@
 int main()
 {
 	printf("%d\n", glfwInit());
+	GLFWwindow* window = glfwCreateWindow(100, 100, "ahh", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.f, 1.f, 0.f, 0.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
